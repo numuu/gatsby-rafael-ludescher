2a6353554aa2c941f65c5d5b772a585371c3dbf1
rafael ludescher <lude.raf@gmail.com>
2022-02-04 13:07:01 +0100

    responsive - index 2

--- src/components/IndexLayout.js
+++ src/components/IndexLayout.js
@@ -12,12 +12,13 @@ import * as layoutStyles from './_Layout.module.scss'
 import StyledBackgroundSection from './StyledBackgroundSection'
 
 const IndexLayout = (props) => {
+    var styleBool = true;
     return (
         <div>
             <div className={layoutStyles.wrapper}>
                 <div className={layoutStyles.indexcontent}>
-                    <Title title = {props.title}/>
-                    <Footer styleBool={true}/>
+                    <Title title = {props.title} styleBool={styleBool}/>
+                    <Footer styleBool={styleBool}/>
                 </div>
             </div>
             <StyledBackgroundSection fileName = {props.fileName} />
--- src/components/Title.js
+++ src/components/Title.js
@@ -4,11 +4,19 @@ import * as titleStyles from './_Title.module.scss'
 
 const Title = (props) => {
     //debugger;
-    return (
+    if(props.styleBool === true){
+      return (
+        <header className={titleStyles.indextitlecontainer}>
+          <h1 className={titleStyles.indextitle}>{props.title}</h1> 
+        </header>
+      )
+    } else {
+      return (
         <header className={titleStyles.titlecontainer}>
           <h1 className={titleStyles.title}>{props.title}</h1> 
         </header>
-    )
+      )
+    }
 }
 
 export default Title;
